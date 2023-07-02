4b784588c3fe9a4f8528fa48c919346c7ff38eb7
ajjdin <123256583+ajjdin@users.noreply.github.com>
2023-07-02 02:07:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+// Get references to the calculator screen elements
+const expressionDisplay = document.querySelector(".expression");
+const resultDisplay = document.querySelector(".result");
+
+// Get references to all the buttons
+const buttons = document.querySelectorAll(".button");
+
+// Store the current expression and result
+let expression = "";
+let result = "";
+
+// Add event listeners to the buttons
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const buttonText = button.textContent;
+
+    if (buttonText === "C") {
+      // Clear the expression and result
+      expression = "";
+      result = "";
+    } else if (buttonText === "CE") {
+      // Clear the last entry from the expression
+      expression = expression.slice(0, -1);
+    } else if (buttonText === "=") {
+      // Evaluate the expression
+      try {
+        result = eval(expression);
+        expression = `${expression} =`;
+      } catch (error) {
+        result = "Error";
+        expression = "";
+      }
+    } else {
+      // Append the button value to the expression
+      if (buttonText === "*" || buttonText === "/") {
+        expression += ` ${buttonText} `;
+      } else {
+        expression += buttonText;
+      }
+    }
+
+    // Update the display
+    expressionDisplay.textContent = expression;
+    resultDisplay.textContent = result;
+  });
+});
