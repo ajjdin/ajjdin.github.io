cd6eae35c7cf3c6cdfec3008470e9d13b82bcb94
ajjdin <123256583+ajjdin@users.noreply.github.com>
2023-07-02 13:32:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const expressionDisplay = document.querySelector(".expression");
+const resultDisplay = document.querySelector(".result");
+
+const buttons = document.querySelectorAll(".button");
+
+let expression = "";
+let result = "";
+
+let lastButtonClicked = null;
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const buttonText = button.textContent;
+
+    if (buttonText === "C") {
+      expression = "";
+      result = "0";
+      lastButtonClicked = null;
+    } else if (buttonText === "CE") {
+      expression = expression.slice(0, -1);
+      lastButtonClicked = null;
+    } else if (buttonText === "=") {
+      try {
+        result = eval(expression);
+        expression = `${expression}`;
+        lastButtonClicked = null;
+      } catch (error) {
+        result = "Error";
+        expression = "";
+        lastButtonClicked = null;
+      }
+    } else {
+      if (
+        lastButtonClicked &&
+        (lastButtonClicked === "+" ||
+          lastButtonClicked === "-" ||
+          lastButtonClicked === "*" ||
+          lastButtonClicked === "/")
+      ) {
+        if (buttonText === "+" || buttonText === "-" || buttonText === "*" || buttonText === "/") {
+          expression = expression.slice(0, -2); // Remove the last number and the space
+        } else {
+          expression = expression.slice(0, -1); // Remove the space after the last number
+        }
+      }
+
+      expression += ` ${buttonText} `;
+      lastButtonClicked = buttonText;
+    }
+
+    expressionDisplay.textContent = expression;
+    resultDisplay.textContent = result;
+  });
+});
